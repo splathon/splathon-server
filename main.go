b74415a8218860bc174e2c409e60753e11a19c67
haya14busa <haya14busa@gmail.com>
2019-04-12 09:51:15 +0000

    Copy main file
    cp ./swagger/cmd/splathon-server/main.go ./main.go

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	loads "github.com/go-openapi/loads"
+	flags "github.com/jessevdk/go-flags"
+	"github.com/splathon/splathon-server/swagger/restapi"
+	"github.com/splathon/splathon-server/swagger/restapi/operations"
+)
+
+func main() {
+
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewSplathonAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+
+	parser := flags.NewParser(server, flags.Default)
+	parser.ShortDescription = "SplathonAPI"
+	parser.LongDescription = "Splathonで使うAPI"
+
+	server.ConfigureFlags()
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	server.ConfigureAPI()
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+
+}
