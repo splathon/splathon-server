53af9c06ec2a7c62d38d53c8fe4d89952cea0f7f
haya14busa <haya14busa@gmail.com>
2019-04-13 11:58:29 +0000

    Return NOT_FOUND when team not found

--- splathon/swagutils/error_responder.go
+++ splathon/swagutils/error_responder.go
@@ -3,7 +3,7 @@ package swagutils
 import (
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/swag"
-	"github.com/splathon/splathon-server/splathon"
+	"github.com/splathon/splathon-server/splathon/serror"
 	"github.com/splathon/splathon-server/swagger/models"
 	"github.com/splathon/splathon-server/swagger/restapi/operations/result"
 )
@@ -11,7 +11,7 @@ import (
 func Error(err error) middleware.Responder {
 	code := 500
 	msg := err.Error()
-	if splathonErr, ok := err.(*splathon.Error); ok {
+	if splathonErr, ok := err.(*serror.Error); ok {
 		code = splathonErr.Code
 		msg = splathonErr.Message
 	}
