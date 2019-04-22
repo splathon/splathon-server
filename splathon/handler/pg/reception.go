943a0d98ab80751262a956d125a32119f636d759
haya14busa <haya14busa@gmail.com>
2019-04-22 16:37:45 +0000

    Fix reception data

--- splathon/handler/pg/reception.go
+++ splathon/handler/pg/reception.go
@@ -33,20 +33,21 @@ func (h *Handler) GetReception(ctx context.Context, params reception.GetReceptio
 			Description: `QRコードを入退ゲートにかざしご入館・退館ください。
 同伴者様がいる場合は同伴者様にもアプリをインストールしていただくか、Splathon 運営に連絡の上事前にQRコードを印刷してご持参ください。
 `,
-			ShortText: "TODO(haya14busa): こっちに名前、参加費、同伴者(ないし運営チェック)の有無など簡潔に書いてもいいかも。",
+			ShortText: fmt.Sprintf("来客用入館証 (受付番号：%s)", os.Getenv("SPLATHON_BUILDING_RECEPTION_NUMBER")),
 			CodeType:  models.ReceptionCodeCodeTypeQrcode,
-			Code:      slackUserID,
-			QrcodeImg: googleQRCodeImageURL(slackUserID),
+			Code:      os.Getenv("SPLATHON_BUILDING_CODE"),
+			QrcodeImg: os.Getenv("SPLATHON_BUILDING_QRCODE_URL"),
 		},
 		Splathon: &models.ReceptionCode{
 			Name: "会場入場コード",
 			Description: `Splathon 会場でこのQRコードを表示して受付してください。
 TODO(haya14busa): ここに最低限の参加者情報や払うべき金額を事前に表示する。
 `,
-			ShortText: fmt.Sprintf("来客用入館証 (受付番号：%s)", os.Getenv("SPLATHON_BUILDING_RECEPTION_NUMBER")),
+			ShortText: "TODO(haya14busa): こっちに名前、参加費、同伴者(ないし運営チェック)の有無など簡潔に書いてもいいかも。",
 			CodeType:  models.ReceptionCodeCodeTypeQrcode,
-			Code:      os.Getenv("SPLATHON_BUILDING_CODE"),
-			QrcodeImg: os.Getenv("SPLATHON_BUILDING_QRCODE_URL"),
+
+			Code:      slackUserID,
+			QrcodeImg: googleQRCodeImageURL(slackUserID),
 		},
 	}
 	return resp, nil
