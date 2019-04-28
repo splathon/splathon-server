87365920df79a86b90fcce7c805f3a4dbb2f8728
haya14busa <haya14busa@gmail.com>
2019-04-28 14:12:01 +0000

    Add another notice

--- splathon/handler/pg/notice.go
+++ splathon/handler/pg/notice.go
@@ -19,6 +19,12 @@ func dummyNotices() *models.ListNoticesResponse {
 	return &models.ListNoticesResponse{
 		Notices: []*models.Notice{
 			{
+				Text: swag.String(`イベントのしおりは読みましたか？まだの方は最新版をチェック!
+URL: https://splathon.kibe.la/shared/entries/9ff1d58e-f4fb-43f2-a776-ac4d91bd3176
+`),
+				TimestampSec: swag.Int64(1556460633),
+			},
+			{
 				Text:         swag.String(`Splathon#10がいよいよ5月1日に開催されます!`),
 				TimestampSec: swag.Int64(1556453100),
 			},
