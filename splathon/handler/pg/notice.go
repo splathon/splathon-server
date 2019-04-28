e5c1d4f8de1d161c019ccb15fac937b15bcbea69
haya14busa <haya14busa@gmail.com>
2019-04-28 12:05:12 +0000

    Remove dummy notices

--- splathon/handler/pg/notice.go
+++ splathon/handler/pg/notice.go
@@ -21,26 +21,8 @@ func dummyNotices() *models.ListNoticesResponse {
 	return &models.ListNoticesResponse{
 		Notices: []*models.Notice{
 			{
-				Text: swag.String(`(ダミーテキスト) いよいよ明日はSplathon#10ですね！むっちゃドキドキしてきた。。。
-みなさんも今日くらいは練習休んで明日に備えますよね？？？ Google https://www.google.com/ `),
-				TimestampSec: swag.Int64(now.Unix()),
-			},
-			{
-				Text: swag.String(`## Splathon前夜祭　特別メニューのお知らせ
-
-Splathon前夜祭では、参加費は別にお金を払うことによって食べられる特別メニュー「廉価版5桁丼」をご用意します。
-メニュー詳細は以下の記事をご参照ください`),
-				TimestampSec: swag.Int64(now.Add(-time.Minute).Unix()),
-			},
-			{
-				Text: swag.String(`コミュニティチームより、Splathon #10当日のガイドラインが公開されました。
-こちらもしおりと併せてご一読ください。`),
-				TimestampSec: swag.Int64(now.Add(-2 * time.Hour).Unix()),
-			},
-			{
-				Text: swag.String(`splathon#10サイトと、併せて『しおり』公開してます！！
-しおりは参加者全員、一読お願いしますっ 🙏`),
-				TimestampSec: swag.Int64(now.Add(-25 * time.Hour).Unix()),
+				Text:         swag.String(`Splathon#10がいよいよ5月1日に開催されます!`),
+				TimestampSec: swag.Int64(1556453100),
 			},
 		},
 	}
