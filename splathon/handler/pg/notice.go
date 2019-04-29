b4358e587d1211599dc18c91da718d3ae04c61b6
haya14busa <haya14busa@gmail.com>
2019-04-29 11:28:08 +0000

    Disable dummy notices

--- splathon/handler/pg/notice.go
+++ splathon/handler/pg/notice.go
@@ -2,10 +2,8 @@ package pg
 
 import (
 	"context"
-	"os"
 	"time"
 
-	"github.com/go-openapi/swag"
 	"github.com/splathon/splathon-server/swagger/models"
 	"github.com/splathon/splathon-server/swagger/restapi/operations"
 	"github.com/splathon/splathon-server/swagger/restapi/operations/admin"
@@ -14,10 +12,6 @@ import (
 func (h *Handler) ListNotices(ctx context.Context, params operations.ListNoticesParams) (*models.ListNoticesResponse, error) {
 	if _, err := h.getTokenSession(params.XSPLATHONAPITOKEN); err != nil {
 		return nil, err
-	}
-	// TODO(haya14busa): remove.
-	if os.Getenv("SPLATHON_DUMMY_NOTICE") == "1" {
-		return dummyNotices(), nil
 	}
 	eventID, err := h.queryInternalEventID(params.EventID)
 	if err != nil {
@@ -36,21 +30,6 @@ func (h *Handler) ListNotices(ctx context.Context, params operations.ListNotices
 		res.Notices[i] = convertNotice(n)
 	}
 	return res, nil
-}
-
-func dummyNotices() *models.ListNoticesResponse {
-	return &models.ListNoticesResponse{
-		Notices: []*models.Notice{
-			{
-				Text:         swag.String(`すでにしおりは読みましたか？まだの方は最新版をチェック! http://bit.ly/2La6fSR`),
-				TimestampSec: swag.Int64(1556460633),
-			},
-			{
-				Text:         swag.String(`Splathon#10がいよいよ5月1日に開催されます!`),
-				TimestampSec: swag.Int64(1556453100),
-			},
-		},
-	}
 }
 
 func (h *Handler) WriteNotice(ctx context.Context, params admin.WriteNoticeParams) error {
